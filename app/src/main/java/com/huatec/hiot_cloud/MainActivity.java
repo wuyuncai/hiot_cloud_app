66e663ac9e8eedac9d2e1aeb794a256376de85f6
diyici <2934608484@qq.com>
2020-04-13 16:58:00 +0800

    第四次修改测试，17物联网工程25号吴运财

--- app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
+++ app/src/main/java/com/huatec/hiot_cloud/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //第三次测试
+        //第四次测试
     }
 }
